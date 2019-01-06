9eb2a93804190ee03944bcbf6da85a79b7a8a9ab
Olivier ESUKA <oesukam@gmail.com>
2019-01-06 12:19:46 +0200

    Made dynamic menu depending on the platform

--- src/utils/menuTemplate.js
+++ src/utils/menuTemplate.js
@@ -5,59 +5,67 @@ const store = new Store();
 const lang = store.get('lang') || 'fr';
 const trans = require(`../mocks/trans_${lang}`);
 
-const menu = (window) => [
-{
-    label: app.getName(),
-    submenu: [
-      {
-        label: trans.about || 'About',
-        click() {
-          aboutController.show();
-        }
-      },
-      {
-        label: 'Preferences',
-        accelerator: 'cmd+,', // shortcut
-        click () {
-          // settingsController.show();
-          // event sent to render.js
-          window.webContents.send('open-settings')
+const menu = (window) => {
+  const appMenu = [
+    {
+      label: trans.edit || 'Edit',
+      submenu: [
+        { label: trans.copy || 'Copy', role: 'copy'  },
+        { label: trans.paste || 'Paste', role:'paste'  },
+      ]
+    },
+    {
+      label: trans.help || 'Help',
+      submenu: [
+        {
+          label: trans.website || 'Website',
+          click() {
+            shell.openExternal('https://oesukam.github.io/bible-desktop');
+          }
         },
-      },
-    ]
-  },
-  {
-    label: trans.edit || 'Edit',
-    submenu: [
-      { label: trans.copy || 'Copy', role: 'copy'  },
-      { label: trans.paste || 'Paste', role:'paste'  },
-    ]
-  },
-  {
-  label: 'View',
-    submenu: [
-      {
-        role: 'reload'
-      },
-      {
-        role: 'toggledevtools'
-      },
-      {
-        type: 'separator'
-      },
-    ]
-  },
-  {
-    label: trans.help || 'Help',
-    submenu: [
-      {
-        label: trans.website || 'Website',
-        click() {
-          shell.openExternal('https://oesukam.github.io/bible');
-        }
-      },
-    ]
+      ]
+    }
+  ];
+  // Check if the app runs on MacOS
+  if (process.platform === 'darwin') {
+    appMenu.unshift({
+      label: app.getName(),
+      submenu: [
+        {
+          label: trans.about || 'About',
+          click() {
+            aboutController.show();
+          }
+        },
+        {
+          label: 'Preferences',
+          accelerator: 'cmd+,', // shortcut
+          click () {
+            // event sent to render.js
+            window.webContents.send('open-settings')
+          },
+        },
+      ]
+    })
+  };
+
+  if (process.env.NODE_ENV === 'dev') {
+    appMenu.push({
+      label: 'Dev',
+      submenu: [
+        {
+          role: 'reload'
+        },
+        {
+          role: 'toggledevtools'
+        },
+        {
+          type: 'separator'
+        },
+      ]
+    });
   }
-]
+  return appMenu;
+}
 
 module.exports = menu;
