66d4007ebef8fab129871e3c5d0cc10bbc4e0358
Olivier ESUKA <oesukam@gmail.com>
2019-01-06 11:46:24 +0200

    Delete files from gh-pages

--- src/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-const getFormattedDate = (date = new Date) => {
-  const str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  return str;
-}
-
-module.exports = {
-  getFormattedDate,
-};
